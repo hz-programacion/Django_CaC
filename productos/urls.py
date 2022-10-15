42d5416f7525f084daeac0d99f95d5910053975f
fabyolalera <fabyolalera@gmail.com>
2022-10-14 21:29:32 -0300

    modificaciones en frontend

--- productos/urls.py
+++ productos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 from productos.views import lista_clases, galeria, contacto
 
 urlpatterns = [
+    path('',views.index,name='inicio'),
     # path('saludar/',saludar, name='saludar'),
     # path('saludando/<str:nombre>', saludar_nombre, name='saludando'),
     path('lista-clases/', lista_clases, name='lista_clases'),
